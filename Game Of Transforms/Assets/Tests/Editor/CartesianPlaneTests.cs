e0d50fda4089b837922a38810973d1eb2b696c82
Marco Ganassin <marcoganassin96@gmail.com>
2019-03-08 14:50:00 +0100

    Add test Throws_Exception_On_Cartesian_Plane_With_Size_Not_Greater_Than_0 in red phase

--- Game Of Transforms/Assets/Tests/Editor/CartesianPlaneTests.cs
+++ Game Of Transforms/Assets/Tests/Editor/CartesianPlaneTests.cs
@@ -1,5 +1,22 @@
-﻿internal class CartesianPlaneTests
-{
+﻿using NUnit.Framework;
+using System;
 
+internal class CartesianPlaneTests
+{
+    [Test]
+    public void Throws_Exception_On_Cartesian_Plane_With_Size_Not_Greater_Than_0()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => new CartesianPlane(0));
+        Assert.Throws<ArgumentOutOfRangeException>(() => new CartesianPlane(-1));
+    }
 }
 
+internal class CartesianPlane
+{
+    private int size;
+
+    public CartesianPlane(int size)
+    {
+        this.size = size;
+    }
+}
