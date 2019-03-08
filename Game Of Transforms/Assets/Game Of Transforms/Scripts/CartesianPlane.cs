869051732c0d157adc73aaa5cb533560ec931d59
Marco Ganassin <marcoganassin96@gmail.com>
2019-03-08 15:31:21 +0100

    Add Assets/Game Of Transforms/Scripts/CartesianPlane.cs

---
+++ Game Of Transforms/Assets/Game Of Transforms/Scripts/CartesianPlane.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GameOfTransforms
+{
+    public class CartesianPlane
+    {
+        private int size;
+
+        public CartesianPlane(int size)
+        {
+            if ( size <= 0 )
+            {
+                throw new ArgumentOutOfRangeException("Size must be greater than 0");
+            }
+            this.size = size;
+        }
+    }
+}
---
+++ Game Of Transforms/Assets/Game Of Transforms/Tests/Editor/CartesianPlaneTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace GameOfTransforms.Tests
+{
+    internal class CartesianPlaneTests
+    {
+        [Test]
+        public void Throws_Exception_On_Cartesian_Plane_With_Size_Not_Greater_Than_0()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new CartesianPlane(0));
+            Assert.Throws<ArgumentOutOfRangeException>(() => new CartesianPlane(-1));
+        }
+    }
+}
