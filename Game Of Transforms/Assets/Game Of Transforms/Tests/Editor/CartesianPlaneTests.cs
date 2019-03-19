96c1514db77e5118c9e47d52d06733d392875a9a
Marco Ganassin <marcoganassin96@gmail.com>
2019-03-19 22:08:05 +0100

    Add test CartesianPlaneTests/The_Constructor/_1_Does_Not_Throw_Exception_On_Cartesian_Plane_With_Size_Greater_Than_0 in green phase

--- Game Of Transforms/Assets/Game Of Transforms/Tests/Editor/CartesianPlaneTests.cs
+++ Game Of Transforms/Assets/Game Of Transforms/Tests/Editor/CartesianPlaneTests.cs
@@ -19,6 +19,18 @@ namespace GameOfTransforms.Tests
             }
 
             #endregion
+
+            #region _1_Does_Not_Throw_Exception_On_Cartesian_Plane_With_Size_Greater_Than_0
+
+            [TestCase(1)]
+            [TestCase(2)]
+            [TestCase(20)]
+            public void _1_Does_Not_Throw_Exception_On_Cartesian_Plane_With_Size_Greater_Than_0 (int size)
+            {
+                Assert.DoesNotThrow(() => new CartesianPlaneController(size));
+            }
+
+            #endregion
         }
     }
 }
