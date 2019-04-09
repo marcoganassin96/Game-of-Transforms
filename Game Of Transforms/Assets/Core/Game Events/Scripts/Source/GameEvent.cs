2b302a765a390b0534314c940529c249a7570a5b
Marco Ganassin <marcoganassin96@gmail.com>
2019-04-09 17:21:04 +0200

    Add Core/Game Events

---
+++ Game Of Transforms/Assets/Core/Game Events/Scripts/Source/GameEvent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core.GameEvents
+{
+    [CreateAssetMenu(menuName = "Core/Game Event", fileName = "Game Event")]
+    public class GameEvent : ScriptableObject
+    {
+        private List<GameEventListener> listeners = new List<GameEventListener>();
+
+        public void Raise ()
+        {
+            foreach (var listener in listeners)
+            {
+                listener.OnEventRaised();
+            }
+        }
+
+        public void AddListener (GameEventListener listener)
+        {
+            listeners.Add(listener);
+        }
+
+        public void RemoveListener (GameEventListener listener)
+        {
+            listeners.Remove(listener);
+        }
+    }
+}
---
+++ Game Of Transforms/Assets/Core/Game Events/Scripts/Source/GameEventListener.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Core.GameEvents
+{
+    public class GameEventListener : MonoBehaviour
+    {
+        public GameEvent  Event    = default;
+        public UnityEvent Response = default;
+
+        private void OnEnable ()
+        {
+            Event.AddListener(this);
+        }
+
+        private void OnDisable ()
+        {
+            Event.RemoveListener(this);
+        }
+
+        public void OnEventRaised ()
+        {
+            Response.Invoke();
+        }
+    }
+}
