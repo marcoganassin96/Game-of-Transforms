d8de7674e763f3c5675ea8fffdc785ce28a5dd0e
Marco Ganassin <marcoganassin96@gmail.com>
2019-04-17 15:09:28 +0200

    Refactor Scripts/ICartesianPlaneData - CartesianPlaneData - CartesianPlaneGraphics

--- Game Of Transforms/Assets/Game Of Transforms/Cartesian Plane/Scripts/ICartesianPlaneData.cs
+++ Game Of Transforms/Assets/Game Of Transforms/Cartesian Plane/Scripts/ICartesianPlaneData.cs
@@ -1,10 +1,13 @@
-﻿namespace GameOfTransforms
+﻿using UnityEngine;
+
+namespace GameOfTransforms
 {
     public interface ICartesianPlaneData
     {
         #region Shared data
 
         int Size { get; }
+        Vector3 Origin { get; }
 
         #endregion
     }
