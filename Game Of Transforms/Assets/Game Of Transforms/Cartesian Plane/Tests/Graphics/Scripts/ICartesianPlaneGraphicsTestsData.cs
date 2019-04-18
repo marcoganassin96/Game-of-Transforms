fdb210450321bf4d8d8c40b0f9460e71f9cbe7aa
Marco Ganassin <marcoganassin96@gmail.com>
2019-04-18 18:43:02 +0200

    Add Scripts/ICartesianPlaneGraphicsTestsData and add Scripts/Source/CartesianPlaneGraphicsTests - CartesianPlaneGraphicsTestsData - CartesianPlaneGraphicsTestsInstaller - Coordinates

---
+++ Game Of Transforms/Assets/Game Of Transforms/Cartesian Plane/Tests/Graphics/Scripts/ICartesianPlaneGraphicsTestsData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameOfTransforms.GraphicsTests
+{
+    public interface ICartesianPlaneGraphicsTestsData
+    {
+        [SerializeField] GameObject CoordinateMarkerPrefab { get; }
+        [SerializeField] List<Coordinate> Coordinates { get; }
+    }
+}
