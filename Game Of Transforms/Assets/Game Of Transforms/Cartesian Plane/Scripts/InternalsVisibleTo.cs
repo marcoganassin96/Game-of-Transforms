5f779e9403ee8c66f27b5487cadaad31cf9dfecd
Marco Ganassin <marcoganassin96@gmail.com>
2019-04-18 23:52:10 +0200

    Rename assembly definition "GameOfTransform.Tests" to "GameOfTransform.CartesianPlane.Tests"

--- Game Of Transforms/Assets/Game Of Transforms/Cartesian Plane/Scripts/InternalsVisibleTo.cs
+++ Game Of Transforms/Assets/Game Of Transforms/Cartesian Plane/Scripts/InternalsVisibleTo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("GameOfTransforms.Tests")]
+[assembly: InternalsVisibleTo("GameOfTransforms.CartesianPlane.Tests")]
 namespace GameOfTransform.CartesianPlane { }
