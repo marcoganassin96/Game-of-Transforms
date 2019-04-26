11264cf43d234c67ba8d61ce10491aba9e053f80
Marco Ganassin <marcoganassin96@gmail.com>
2019-04-26 10:38:14 +0200

    Delete Scripts/IPolygon and refactor Scripts/Polygon

---
+++ Game Of Transforms/Assets/Game Of Transforms/Transformation/Polygon/Scripts/Polygon.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameOfTransform.Transformation.Polygon
+{
+    public class Polygon : MonoBehaviour
+    {
+        public List<Vector2> points = default;
+
+        public Polygon (List<Vector2> points)
+        {
+            this.points = points;
+        }
+    }
+}
