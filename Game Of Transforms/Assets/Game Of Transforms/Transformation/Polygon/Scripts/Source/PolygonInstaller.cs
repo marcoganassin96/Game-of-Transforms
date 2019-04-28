a8cc001e9f628b21aa92662d59df868723930391
Marco Ganassin <marcoganassin96@gmail.com>
2019-04-28 18:07:15 +0200

    Add Scripts/Source/PolygonInstaller and refactor Scripts/Source/PolygonData - PolygonGraphicsData

---
+++ Game Of Transforms/Assets/Game Of Transforms/Transformation/Polygon/Scripts/Source/PolygonInstaller.cs
@@ -0,0 +1,19 @@
+using GameOfTransforms.Events;
+using UnityEngine;
+using Zenject;
+
+namespace GameOfTransforms.Transformation.Polygon
+{
+    [CreateAssetMenu(fileName = "Polygon Installer", menuName = "Game Of Transforms/Transformation/Polygon/Polygon Installer")]
+    public class PolygonInstaller : ScriptableObjectInstaller<PolygonInstaller>
+    {
+        [SerializeField] private OnNewPolygonData onNewPolygonData = default;
+        [SerializeField] private OnNewPolygonGraphicsData onNewPolygonGraphicsData = default;
+
+        public override void InstallBindings ()
+        {
+            Container.Bind<IOnNewPolygonData>().FromInstance(onNewPolygonData).AsCached();
+            Container.Bind<IOnNewPolygonGraphicsData>().FromInstance(onNewPolygonGraphicsData).AsCached();
+        }
+    }
+}
