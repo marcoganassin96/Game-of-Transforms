656d23a1c940c9428220f73fcde5197a4eb68ffd
Marco Ganassin <marcoganassin96@gmail.com>
2019-04-28 14:18:09 +0200

    Fix AssetMenu in Scripts/Source/OnTransformationData

--- Game Of Transforms/Assets/Game Of Transforms/Events/Scripts/Source/OnTransformationData.cs
+++ Game Of Transforms/Assets/Game Of Transforms/Events/Scripts/Source/OnTransformationData.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 namespace GameOfTransforms.Events
 {
+    [CreateAssetMenu(fileName = "On Transformation Data", menuName = "Game Of Transforms/Events/On Transformation Data")]
     public class OnTransformationData : ScriptableObject, IOnTransformationData
     {
         public TransformationMatrix TransformationMatrix { get; set; } = default;
