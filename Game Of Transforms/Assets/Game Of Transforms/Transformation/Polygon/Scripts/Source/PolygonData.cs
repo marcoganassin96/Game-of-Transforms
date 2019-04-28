c997f0fc1882984f8726a3d1bf6269f1f81777c4
Marco Ganassin <marcoganassin96@gmail.com>
2019-04-28 15:20:53 +0200

    Replaced IPolygon with IPolygonData in Scripts and replaced Polygon with PolygonData in Scripts/Source

---
+++ Game Of Transforms/Assets/Game Of Transforms/Transformation/Polygon/Scripts/Source/PolygonData.cs
@@ -0,0 +1,21 @@
+﻿using GameOfTransforms.Events;
+using System.Collections.Generic;
+using UnityEngine;
+using Vector2 = System.Numerics.Vector2;
+
+namespace GameOfTransforms.Transformation.Polygon
+{
+    [CreateAssetMenu(fileName = "Polygon Data", menuName = "Game Of Transforms/Transformation/Polygon/Polygon Data")]
+    internal class PolygonData : ScriptableObject, IPolygonData
+    {
+        [SerializeField] private List<Vector2> points = default;
+        public List<Vector2> Points => points;
+
+        [SerializeField] private IOnNewPolygonData onNewPolygonData = default;
+
+        internal void OnNewPolygon ()
+        {
+            points = onNewPolygonData.Points;
+        }
+    }
+}
