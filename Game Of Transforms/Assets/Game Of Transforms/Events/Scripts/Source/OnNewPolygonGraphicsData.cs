ee7e9cbee34cf5d1dc5d2b426d1aef5eaaac35bd
Marco Ganassin <marcoganassin96@gmail.com>
2019-04-28 12:26:25 +0200

    Add Scripts/Source/OnNewPolygonData - OnNewPolygonGraphicsData

---
+++ Game Of Transforms/Assets/Game Of Transforms/Events/Scripts/Source/OnNewPolygonGraphicsData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameOfTransforms.Events
+{
+    [CreateAssetMenu(fileName = "On New Polygon Graphics Data", menuName = "Game Of Transforms/Events/On New Polygon Graphics Data")]
+    public class OnNewPolygonGraphicsData : ScriptableObject, IOnNewPolygonGraphicsData
+    {
+        public List<Transform> Points { get; set; }
+    }
+}
