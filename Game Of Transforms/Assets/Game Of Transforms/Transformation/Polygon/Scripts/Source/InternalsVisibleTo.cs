dc773b38cf7116a74cfabbc890259dc14c440a19
Marco Ganassin <marcoganassin96@gmail.com>
2019-04-29 17:07:35 +0200

    Add "GameOfTransforms.Transformation.Transformator.Tests" internals visible in Scripts/Source/InternalsVisibleTo

--- Game Of Transforms/Assets/Game Of Transforms/Transformation/Polygon/Scripts/Source/InternalsVisibleTo.cs
+++ Game Of Transforms/Assets/Game Of Transforms/Transformation/Polygon/Scripts/Source/InternalsVisibleTo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("GameOfTransforms.Transformation.Transformator")]
+[assembly: InternalsVisibleTo("GameOfTransforms.Transformation.Transformator.Tests")]
 namespace GameOfTransforms.Transformation.Polygon { }
