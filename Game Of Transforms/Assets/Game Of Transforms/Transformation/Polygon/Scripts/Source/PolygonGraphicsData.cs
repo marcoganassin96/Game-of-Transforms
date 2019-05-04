abbe335fa765dd1fdcc04a7d14a6ef28b847f5ae
Marco Ganassin <marcoganassin96@gmail.com>
2019-05-04 18:11:11 +0200

    Fix Scripts/IPolygonGraphicsData and fix Scripts/Source/PolygonGraphicsData

--- Game Of Transforms/Assets/Game Of Transforms/Transformation/Polygon/Scripts/Source/PolygonGraphicsData.cs
+++ Game Of Transforms/Assets/Game Of Transforms/Transformation/Polygon/Scripts/Source/PolygonGraphicsData.cs
@@ -1,6 +1,4 @@
-﻿using GameOfTransforms.Events;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using Zenject;
 
 namespace GameOfTransforms.Transformation.Polygon
@@ -8,14 +6,23 @@ namespace GameOfTransforms.Transformation.Polygon
     [CreateAssetMenu(fileName = "Polygon Graphics Data", menuName = "Game Of Transforms/Transformation/Polygon/Polygon Graphics Data")]
     internal class PolygonGraphicsData : ScriptableObject, IPolygonGraphicsData
     {
-        [SerializeField] private List<Transform> points = default;
-        public List<Transform> Points => points;
+        [SerializeField] private Points2LogicCoordinates points2LogicCoordinates = default;
+        public Points2LogicCoordinates Points2LogicCoordinates => points2LogicCoordinates;
 
-        [Inject] private IOnNewPolygonGraphicsData onNewPolygonGraphicsData = default;
+        [Inject] private IPolygonData polygonData = default;
+        [Inject] private IPolygonGraphicsSettings polygonGraphicsSettings = default;
 
         public void OnNewPolygon ()
         {
-            points = onNewPolygonGraphicsData.Points;
+            points2LogicCoordinates = new Points2LogicCoordinates();
+            char label = 'A';
+            foreach (Vector2 coordinate in polygonData.Points)
+            {
+                GameObject point = Instantiate(polygonGraphicsSettings.GraphicalPointPrefab);
+                point.name = "Point " + label;
+                points2LogicCoordinates.Add(point.transform, coordinate);
+                label++;
+            }
         }
     }
 }
