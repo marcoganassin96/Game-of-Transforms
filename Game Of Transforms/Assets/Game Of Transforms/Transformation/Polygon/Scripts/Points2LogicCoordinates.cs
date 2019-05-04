7442eb0c149a44865c58af0ea1104b74f4aa3791
Marco Ganassin <marcoganassin96@gmail.com>
2019-05-04 17:03:53 +0200

    Add Scripts/Points2LogicCoordinates

---
+++ Game Of Transforms/Assets/Game Of Transforms/Transformation/Polygon/Scripts/Points2LogicCoordinates.cs
@@ -0,0 +1,9 @@
+﻿using RotaryHeart.Lib.SerializableDictionary;
+using System;
+using UnityEngine;
+
+namespace GameOfTransforms.Transformation.Polygon
+{
+    [Serializable]
+    public class Points2LogicCoordinates : SerializableDictionaryBase<Transform, Vector2> { };
+}
