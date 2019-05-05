975dd0620ff8e33a3592c61e5243a1475feae910
Marco Ganassin <marcoganassin96@gmail.com>
2019-05-05 18:56:58 +0200

    Add Scripts/ITransformationSettings and Scripts/Source/TransformationSettings

---
+++ Game Of Transforms/Assets/Game Of Transforms/Transformation/Scripts/Source/TransformationSettings.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace GameOfTransforms.Transformation
+{ 
+    [CreateAssetMenu(fileName = "Transformation Settings", menuName = "Game Of Transforms/Transformation/Transformation Settings")]
+    internal class TransformationSettings: ScriptableObject, ITransformationSettings
+    {
+        [SerializeField] private Trasformation2IdentityElementDict trasformation2IdentityElement = default;
+        public Trasformation2IdentityElementDict Trasformation2IdentityElement => trasformation2IdentityElement;
+    }
+}
