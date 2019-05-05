772e4d5d0d00f909e9c6ad4022ef795bcf370367
Marco Ganassin <marcoganassin96@gmail.com>
2019-05-05 20:19:30 +0200

    Add Scripts/Source/GraphicsTransformatorTests

--- Game Of Transforms/Assets/Game Of Transforms/Transformation/Transformator/Scripts/Source/InternalsVisibleTo.cs
+++ Game Of Transforms/Assets/Game Of Transforms/Transformation/Transformator/Scripts/Source/InternalsVisibleTo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("GameOfTransforms.Transformation.Transformator.Tests")]
+[assembly: InternalsVisibleTo("GameOfTransforms.Transformation.Transformator.Tests.Graphics")]
 namespace GameOfTransforms.Transformation.Transformator { }
